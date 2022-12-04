2fda96e2414ddd802e70ceea797d04a8268aa71c
Mohammed Maqsood shaik <shaikmdmaqsood@gmail.com>
2022-12-04 15:05:08 +0100

    probing through the training

--- variable_length_bias_wop.py
+++ variable_length_bias_wop.py
@@ -9,13 +9,18 @@ import skimage.measure
 import wandb
 import torch
 torch.cuda.empty_cache()
+#import plt
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+#set seed for reproducibility
 def set_seed(seed):
+    # Set the random seed manually for reproducibility.
     torch.manual_seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 set_seed(42)
 wandb.init(project='variable_length_bias_wop')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,22 +28,17 @@ config = wandb.config
 config.max_duration_en = 7.0
 config.max_duration_cn = 3.0
 config.max_duration_fr = 5.0
-config.model = "xlsr"
-# config.max_duration_en = 3.0
-# config.max_duration_cn = 3.0
-# config.max_duration_fr = 3.0
 metric = load_metric("accuracy")
 model_checkpoint = "facebook/wav2vec2-base"
 
-batch_size = 16
-# feature_extractor = AutoFeatureExtractor.from_pretrained(model_checkpoint)
-max_duration = 10.0  # seconds
+batch_size = 64
+
 
 dataset_name = "fleurs"
 configs = ['en_us','cmn_hans_cn','fr_fr']
-#,'de_de','nl_nl']
+
 labels =["English","Mandarin Chinese","French"]
-#,"German","Dutch"]
+
 
 label2id, id2label,label2id_int = dict(), dict(),dict()
 
@@ -46,7 +46,7 @@ for i, label in enumerate(labels):
     label2id[label] = str(i)
     id2label[str(i)] = label
     label2id_int[label] = i
-from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer,Wav2Vec2ForSequenceClassification,Wav2Vec2Config
+from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer,Wav2Vec2ForSequenceClassification,Wav2Vec2Config,Wav2Vec2FeatureExtractor
 
 
 num_labels = len(id2label)
@@ -60,9 +60,9 @@ cnf = Wav2Vec2Config()
 cnf.num_labels=num_labels
 cnf.label2id=label2id
 cnf.id2label=id2label
-
+cnf.output_hidden_states=True
 best_model = Wav2Vec2ForSequenceClassification(cnf)
-feature_extractor = AutoFeatureExtractor.from_pretrained(model_checkpoint)
+feature_extractor = Wav2Vec2FeatureExtractor()
 # del best_model_config
 torch.cuda.empty_cache()
 # best_model = AutoModelForAudioClassification.from_pretrained(
@@ -98,114 +98,177 @@ def compute_metrics(eval_pred):
     """Computes accuracy on a batch of predictions"""
     predictions = np.argmax(eval_pred.predictions, axis=1)
     return metric.compute(predictions=predictions, references=eval_pred.label_ids)
+class Load_dataset():
+    def __init__(self,dataset_name,configs,language_duration,feature_extractor,label2id_int):
+        self.dataset_name = dataset_name
+        self.configs = configs
+        self.language_duration = language_duration
+        self.feature_extractor = feature_extractor
+        self.label2id_int = label2id_int
 
-
-dataset_train_en = load_dataset("google/fleurs","en_us",split = "train")
-dataset_train_cn = load_dataset("google/fleurs","cmn_hans_cn",split = "train")
-dataset_train_fr = load_dataset("google/fleurs","fr_fr",split = "train")
-dataset_test_en = load_dataset("google/fleurs","en_us",split = "test")
-dataset_test_cn = load_dataset("google/fleurs","cmn_hans_cn",split = "test")
-dataset_test_fr = load_dataset("google/fleurs","fr_fr",split = "test")
+    def preprocess_function_language(self,examples):
+        audio_arrays = [x["array"] for x in examples["audio"]]
+        inputs = self.feature_extractor(
+        audio_arrays, 
+        sampling_rate=self.feature_extractor.sampling_rate, 
+        max_length=int(self.feature_extractor.sampling_rate *self.language_duration), 
+        truncation=True
+        )
+        inputs["labels"] = [label2id_int[image] for image in examples["language"]]
+        return inputs
+    def train_split(self):
+        dataset_loaded_train = load_dataset(self.dataset_name, self.configs, split="train")
+    
+        encoded_dataset_train = dataset_loaded_train.map(self.preprocess_function_language,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
+        
+        return encoded_dataset_train
+    def test_split(self):
+        dataset_loaded_test = load_dataset(self.dataset_name, self.configs, split="test")
+        encoded_dataset_test = dataset_loaded_test.map(self.preprocess_function_language,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
+        return encoded_dataset_test
 def preprocess_function(examples):
     max_duration = max(config.max_duration_en,config.max_duration_cn,config.max_duration_fr)
     audio_arrays = [x for x in examples["input_values"]]
     inputs = feature_extractor(
         audio_arrays, 
         sampling_rate=feature_extractor.sampling_rate, 
-        max_length=int(feature_extractor.sampling_rate * max_duration), 
-        padding="max_length" 
+            max_length=int(feature_extractor.sampling_rate * max_duration), 
+            padding="max_length" 
     )
-    # inputs["labels"] = [label2id_int[image] for image in examples["language"]]
     return inputs
-
-def preprocess_function_en(examples):
-    
-    audio_arrays = [x["array"] for x in examples["audio"]]
-    inputs = feature_extractor(
-        audio_arrays, 
-        sampling_rate=feature_extractor.sampling_rate, 
-        max_length=int(feature_extractor.sampling_rate * config.max_duration_en ), 
-        truncation=True
-    )
-    inputs["labels"] = [label2id_int[image] for image in examples["language"]]
-    return inputs
-def preprocess_function_fr(examples):
-    audio_arrays = [x["array"] for x in examples["audio"]]
-    inputs = feature_extractor(
-        audio_arrays, 
-        sampling_rate=feature_extractor.sampling_rate, 
-        max_length=int(feature_extractor.sampling_rate * config.max_duration_fr), 
-        truncation=True
-    )
-    inputs["labels"] = [label2id_int[image] for image in examples["language"]]
-    return inputs
-def preprocess_function_cn(examples):
-    
-    audio_arrays = [x["array"] for x in examples["audio"]]
-    inputs = feature_extractor(
-        audio_arrays, 
-        sampling_rate=feature_extractor.sampling_rate, 
-        max_length=int(feature_extractor.sampling_rate * config.max_duration_cn), 
-        truncation=True
-    )
-    inputs["labels"] = [label2id_int[image] for image in examples["language"]]
-    return inputs
-encoded_dataset_fr = dataset_train_fr.map(preprocess_function_fr,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-encoded_dataset_en = dataset_train_en.map(preprocess_function_en, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-encoded_dataset_cn = dataset_train_cn.map(preprocess_function_cn, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-encoded_dataset_test_fr = dataset_test_fr.map(preprocess_function_fr,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-encoded_dataset_test_en = dataset_test_en.map(preprocess_function_en, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-encoded_dataset_test_cn = dataset_test_cn.map(preprocess_function_cn, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
-
+train_dataset_list = []
+test_dataset_list = []
+for config_lang,duration in zip(configs,[config.max_duration_en,config.max_duration_cn,config.max_duration_fr]):
+    dataset_obj = Load_dataset("google/fleurs",config_lang,duration,feature_extractor,label2id_int)
+    train_dataset_list.append(dataset_obj.train_split())
+    test_dataset_list.append(dataset_obj.test_split())
 dataset_train_combined= concatenate_datasets(
-        [encoded_dataset_en,encoded_dataset_cn,encoded_dataset_fr]
+        train_dataset_list
     )
 
 dataset_test_combined= concatenate_datasets(
-        [encoded_dataset_test_en,encoded_dataset_test_cn,encoded_dataset_test_fr]
+        test_dataset_list
     )
-
+equal_length = []
+for config_lang_same,duration_same in zip(configs,[7.0,7.0,7.0]):
+    dataset_obj_same = Load_dataset("google/fleurs",config_lang_same,duration_same,feature_extractor,label2id_int)
+    equal_length.append(dataset_obj_same.test_split())
+dataset_test_equal_length= concatenate_datasets(
+        equal_length
+    )
 encoded_dataset_train = dataset_train_combined.map(preprocess_function,batched=True)
 encoded_dataset_test = dataset_test_combined.map(preprocess_function,batched=True)
+encoded_dataset_test_equal_length = dataset_test_equal_length.map(preprocess_function,batched=True)
 
-# encoded_dataset_train.set_format("torch")
-# encoded_dataset_test.set_format("torch")
-#split train set into train and validation set using 80/20 split
-# train_dataset, validation_dataset = train_test_split(train_dataset, test_size=0.2, random_state=42)
-# train_dataset.set_format("torch")
-# validation_dataset.set_format("torch")
-# eval_dataloader = DataLoader(encoded_dataset_test, batch_size=16)
-# train_dataloader = DataLoader(encoded_dataset_train, batch_size=16)
-# test_dataloader = DataLoader(test_dataset, batch_size=16)
-trainer = Trainer(
-    best_model,
-    args,
-    train_dataset=encoded_dataset_train,
-    eval_dataset=encoded_dataset_test,
-    tokenizer=feature_extractor,
-    compute_metrics=compute_metrics
-)
-import gc
-del dataset_train_en
-del dataset_train_cn
-del dataset_train_fr
-del dataset_test_en
-del dataset_test_cn
-del dataset_test_fr
-del encoded_dataset_en
-del encoded_dataset_cn
-del encoded_dataset_fr
-del encoded_dataset_test_en
-del encoded_dataset_test_cn
-del encoded_dataset_test_fr
-del dataset_train_combined
-del dataset_test_combined
-gc.collect()
+encoded_dataset_train.set_format("torch")
+encoded_dataset_test.set_format("torch")
+encoded_dataset_test_equal_length.set_format("torch")
+eval_dataloader = DataLoader(encoded_dataset_test, batch_size=16)
+eval_dataloader_hidden = DataLoader(encoded_dataset_test_equal_length, batch_size=16,shuffle=True,drop_last=True)
+train_dataloader = DataLoader(encoded_dataset_train, batch_size=16,shuffle=True,drop_last=True)
 
-torch.cuda.empty_cache()
+
+config.epochs = 2
+from transformers import get_linear_schedule_with_warmup, AdamW
+optimizer = AdamW(best_model.parameters(), lr=3e-5)
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=len(train_dataloader)*config.epochs*0.1, num_training_steps=len(train_dataloader)*config.epochs)
+best_model = best_model.to(device)
+
+from sklearn.linear_model import LogisticRegression
+#import train_test_split
+from sklearn.model_selection import train_test_split
+
+criterion = torch.nn.CrossEntropyLoss()
+
+#disable gradient calculation for the function
+@torch.no_grad()
+def eval_func(eval_dataloader, best_model,length):
+    n_correct = 0
+    n_samples = 0
+    for batch in eval_dataloader:
+        best_model.eval()
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = best_model(batch["input_values"])
+        loss = outputs.loss
+        _, predicted = torch.max(outputs.logits, 1)
+        n_samples += 1
+        n_correct += (predicted == batch["labels"]).sum().item()
+    acc = 100.0 * n_correct / len(encoded_dataset_test)
+    print(f'Accuracy of the network on the validation images: {acc} %')
+    #log to wandb
+    wandb.log({f"Accuracy on validation of {length}": acc})
+@torch.no_grad()
+def eval_hidden_func(eval_dataloader_hidden, best_model,length):
+    labels_p= torch.tensor([])
+    logits_p = torch.tensor([])
+    d = {}
+    
+    for x in range(13):
+        d[f"hidden_state_{x}"] = torch.tensor([])
+    for batch in eval_dataloader_hidden:
+        best_model.eval()
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = best_model(batch["input_values"])
+        labels_s = batch["labels"].to("cpu")
+        labels_p = torch.cat((labels_p,labels_s),0)
+        logits_p = torch.cat((logits_p,outputs.logits.to("cpu")),0)
+        for num,hidden_state in enumerate(outputs.hidden_states):
+                #LogisticRegression from sklearn to predict the language
+                hidden_state = hidden_state.to("cpu")
+            
+                hidden_state = hidden_state.mean(dim=1)
+                hidden_state = hidden_state.reshape(hidden_state.shape[0], -1)
+                d[f"hidden_state_{num}"] = torch.cat((d[f"hidden_state_{num}"],hidden_state),0)
+    for v in range(13):
+        #plot the hidden states in 2 dimension using TSNE with labels_p as labels
+        tsne = TSNE(n_components=2, random_state=0)
+        X_2d = tsne.fit_transform(d[f"hidden_state_{v}"])
+        plt.figure(figsize=(10, 10))
+        plt.scatter(X_2d[:, 0], X_2d[:, 1], c=labels_p)
+        
+        #store the plot in wandb as image
+        wandb.log({f"TSNE plot of hidden state {v} of {length}": wandb.Image(plt)})
+        # X_train, X_test, y_train, y_test = train_test_split(d[f"hidden_state_{v}"], labels_p, test_size=0.2, random_state=42)
+        # #train logistic regression model
+        # clf = LogisticRegression(random_state=0,max_iter=400).fit(X_train, y_train)
+        # score = clf.score(X_test, y_test)
+        # print(f"accuracy of hidden state level {v} is {score}")
+        # #log accuracy of each hidden state level to wandb
+        # wandb.log({f"accuracy of hidden state level {v} for dataset of same size":score})
+    #plot the logits in 2 dimension using TSNE with labels_p as labels
+    tsne = TSNE(n_components=2, random_state=0)
+    X_2d = tsne.fit_transform(logits_p)
+    plt.figure(figsize=(10, 10))
+    plt.scatter(X_2d[:, 0], X_2d[:, 1], c=labels_p)
+    #store the plot in wandb as image
+    wandb.log({f"TSNE plot of logits of {length}": wandb.Image(plt)})
+for epoch in range(1, config.epochs + 1):
+    print(f"Epoch {epoch}")
+    n_correct = 0
+    n_samples = 0
+    for batch_iter,batch in enumerate(train_dataloader):
+        best_model.train()
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = best_model(batch["input_values"])
+        loss = criterion(outputs["logits"], batch["labels"]) 
+        print(f"loss in batch {batch_iter+1} is {loss}")
+        #log loss using wandb
+        wandb.log({"loss":loss})
+        if batch_iter % 10 == 0:
+            with torch.no_grad():
+                eval_hidden_func(eval_dataloader_hidden, best_model,"same")
+                eval_hidden_func(eval_dataloader, best_model,"different")
+                eval_func(eval_dataloader_hidden, best_model,"same")
+                eval_func(eval_dataloader, best_model,"different")
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+        optimizer.zero_grad()
+    with torch.no_grad():
+        eval_func(eval_dataloader, best_model)
+
+breakpoint()
 print(trainer.train())
-
-
-trainer.save_model( f"/wop/{model_name}_bestmodel")
+trainer.save_model( f"/pretrained/{model_name}_bestmodel")
 print(trainer.evaluate())
 
