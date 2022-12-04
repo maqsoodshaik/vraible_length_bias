42a926456a157403568bc0959dcf16adab4d337d
Mohammed Maqsood shaik <shaikmdmaqsood@gmail.com>
2022-12-04 15:01:45 +0100

    variable length with debiasing

--- feature_extract.py
+++ feature_extract.py
@@ -1,67 +1,105 @@
-from datasets import get_dataset_config_names
-import pickle
-import torch
-import shutil
+from datasets import load_dataset, load_metric,concatenate_datasets,Dataset
+from transformers import AutoFeatureExtractor
+from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer
 from torch.utils.data import DataLoader
-import os
-from transformers import AutoFeatureExtractor, Wav2Vec2Model
-from transformers.models.wav2vec2.modeling_wav2vec2 import _compute_mask_indices
-max_duration = 10.0
+import torch
+import numpy as np
+import skimage.measure
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-from datasets import load_dataset, concatenate_datasets
 
-feature_extractor = AutoFeatureExtractor.from_pretrained(
-    "facebook/wav2vec2-large-xlsr-53"
+metric = load_metric("accuracy")
+model_checkpoint = "/pretrained/wav2vec2-large-xlsr-53en_uscmn_hans_cnfr_fr7.0_3.0_5.0fleurs_bestmodel"
+batch_size = 64
+feature_extractor = AutoFeatureExtractor.from_pretrained(model_checkpoint)
+max_duration_en_1 = 7.0
+max_duration_en_2 = 3.0
+max_duration_en_3 = 5.0
+
+dataset_name = "fleurs"
+configs = ['en_us']
+#,'de_de','nl_nl']
+labels =[max_duration_en_1,max_duration_en_2,max_duration_en_3]
+#,"German","Dutch"]
+label2id, id2label,label2id_int = dict(), dict(),dict()
+for i, label in enumerate(labels):
+    label2id[label] = str(i)
+    id2label[str(i)] = label
+    label2id_int[label] = i
+best_model = AutoModelForAudioClassification.from_pretrained(
+    model_checkpoint,output_hidden_states=True
 )
-model = Wav2Vec2Model.from_pretrained("facebook/wav2vec2-large-xlsr-53").to(
-    device
-)
-configs = get_dataset_config_names("google/fleurs")
-configs = ["en_us","nl_nl","de_de","fr_fr","es_419"]
-def preprocess_function_f(examples):
-    audio_arrays = [x["array"] for x in examples["audio"]]
+def preprocess_function(examples):
+    max_duration = max(max_duration_en_1,max_duration_en_2,max_duration_en_3)
+    audio_arrays = [x for x in examples["input_values"]]
     inputs = feature_extractor(
         audio_arrays, 
         sampling_rate=feature_extractor.sampling_rate, 
         max_length=int(feature_extractor.sampling_rate * max_duration), 
+        padding="max_length" 
+    )
+    # inputs["labels"] = [label2id_int[image] for image in examples["language"]]
+    return inputs
+def preprocess_function_en_1(examples):
+    
+    audio_arrays = [x["array"] for x in examples["audio"]]
+    inputs = feature_extractor(
+        audio_arrays, 
+        sampling_rate=feature_extractor.sampling_rate, 
+        max_length=int(feature_extractor.sampling_rate * max_duration_en_1  ), 
+        truncation=True
+    )
+    inputs["labels"] = [label2id_int[max_duration_en_1] for image in examples["language"]]
+    return inputs
+def preprocess_function_en_2(examples):
+    audio_arrays = [x["array"] for x in examples["audio"]]
+    inputs = feature_extractor(
+        audio_arrays, 
+        sampling_rate=feature_extractor.sampling_rate, 
+        max_length=int(feature_extractor.sampling_rate * max_duration_en_2), 
         truncation=True,
         padding=True 
     )
+    inputs["labels"] = [label2id_int[max_duration_en_2] for image in examples["language"]]
     return inputs
-print(configs)
-for name in configs:
-    ds_train = load_dataset("google/fleurs", name,split="train")
-    ds_validation = load_dataset("google/fleurs", name, split="validation")
-    ds_test = load_dataset("google/fleurs", name, split="test")
+def preprocess_function_en_3(examples):
+    audio_arrays = [x["array"] for x in examples["audio"]]
+    inputs = feature_extractor(
+        audio_arrays, 
+        sampling_rate=feature_extractor.sampling_rate, 
+        max_length=int(feature_extractor.sampling_rate * max_duration_en_3), 
+        truncation=True,
+        padding=True 
+    )
+    inputs["labels"] = [label2id_int[max_duration_en_3] for image in examples["language"]]
+    return inputs
+dataset_validation_en = load_dataset("google/fleurs","en_us",split = "validation")
+encoded_dataset_validation_en_1 = dataset_validation_en.map(preprocess_function_en_1,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
+encoded_dataset_validation_en_2 = dataset_validation_en.map(preprocess_function_en_2,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
+encoded_dataset_validation_en_3 = dataset_validation_en.map(preprocess_function_en_3,remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription", "gender", "lang_id", "language", "lang_group_id"], batched=True)
 
-    ds = concatenate_datasets(
-        [ds_train,ds_validation,ds_test]
-    )  # ,ds_validation,ds_test,ds_train
-    print(len(ds))
-    ds = ds.map(preprocess_function_f, remove_columns=["id","num_samples", "path", "audio", "transcription", "raw_transcription",  "lang_id", "language", "lang_group_id"], batched=True)
-
-    ds_validation = []
-    ds_test = []
-    projected_states = torch.tensor([])
-    projected_quantized_states = torch.tensor([])
-    hidden_states = torch.tensor([])
-    gender = torch.tensor([])
-    last_hidden_states =torch.tensor([])
-    raw_sequence_length_m =0
-    ds.set_format("torch")
-    eval_dataloader = DataLoader(ds, batch_size=16)
-
-    for batch in eval_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(batch["input_values"])
-        
-        last_hidden_states = torch.cat((last_hidden_states,outputs.last_hidden_state.to("cpu")),dim=0)
-        gender = torch.cat((gender,batch["gender"].to("cpu")),dim=0)
-
-    # torch.save(projected_states,f'feature_extracted/{name}projected_states_all.pt')
-    # torch.save(projected_quantized_states,f'feature_extracted/{name}projected_quantized_states_all.pt')
-    torch.save(gender, f'feature_extracted/{name}gender.pt') 
-    torch.save(last_hidden_states,f'feature_extracted/{name}last_hidden_states.pt')
-    print("Done!")
-
+dataset_validation_combined= concatenate_datasets(
+        [encoded_dataset_validation_en_1,encoded_dataset_validation_en_2,encoded_dataset_validation_en_3]
+    )
+best_model= best_model.wav2vec2
+dataset_validation_combined = dataset_validation_combined.map(preprocess_function,batched=True)
+dataset_validation_combined.set_format("torch")
+eval_dataloader = DataLoader(dataset_validation_combined, batch_size=16)
+pred = torch.tensor([])
+best_model = best_model.to(device)
+labels_p= torch.tensor([])
+domain= torch.tensor([])
+best_model.eval()
+# breakpoint()
+for batch in eval_dataloader:
+    batch = {k: v.to(device) for k, v in batch.items()}
+    with torch.no_grad():
+        outputs = best_model(batch["input_values"])
+        pred_s = outputs.last_hidden_state.to("cpu")
+        pred = torch.cat((pred,pred_s),0)
+        labels_s = batch["labels"].to("cpu")
+        labels_p = torch.cat((labels_p,labels_s),0)
+        # domain_s =  batch["domain"].to("cpu")
+        # domain = torch.cat((domain,domain_s),0)
+torch.save(labels_p, f'feature_extracted/{model_checkpoint.split("/")[-1]}_{configs[0]}_length.pt') 
+torch.save(pred,f'feature_extracted/{model_checkpoint.split("/")[-1]}last_hidden_states.pt')
+print("Done!")
--- variable_length_bias_eval.py
+++ variable_length_bias_eval.py
@@ -1,24 +1,29 @@
 from datasets import load_dataset, load_metric,concatenate_datasets,Dataset
 from transformers import AutoFeatureExtractor
 from transformers import AutoModelForAudioClassification, TrainingArguments, Trainer
+
 from torch.utils.data import DataLoader
 import torch
 import numpy as np
+from typing import Optional, Tuple
 import skimage.measure
 #import weights and bias
 import wandb
+#device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 wandb.init(project='variable_length_bias')
 config = wandb.config
-
+# P = np.load("P_100_EN_length.npy")
+P = np.load("P_100_EN_wav2vec2-large-xlsr-53en_uscmn_hans_cnfr_fr7_length.npy")
 metric = load_metric("accuracy")
 metric_f1 = load_metric("f1")
-model_checkpoint = "/pretrained/wav2vec2-baseen_uscmn_hans_cnfr_fr3.0_3.0_3.0fleurs_bestmodel"
-batch_size = 64
+model_checkpoint = "/pretrained/wav2vec2-large-xlsr-53en_uscmn_hans_cnfr_fr7.0_3.0_5.0fleurs_bestmodel"
+batch_size = 16
 feature_extractor = AutoFeatureExtractor.from_pretrained(model_checkpoint)
 max_duration = 10.0  # seconds
-config.max_duration_en = 7.0
-config.max_duration_fr = 5.0
-config.max_duration_cn = 3.0
+config.max_duration_en = 3.0
+config.max_duration_fr = 7.0
+config.max_duration_cn = 5.0
 
 dataset_name = "fleurs"
 configs = ['en_us','cmn_hans_cn','fr_fr']
@@ -36,6 +41,27 @@ for i, label in enumerate(labels):
 best_model = AutoModelForAudioClassification.from_pretrained(
     model_checkpoint, num_labels=len(labels),label2id=label2id,
     id2label=id2label)
+
+class Model_with_new_head(torch.nn.Module):
+    def __init__(self, model, new_head):
+        super(Model_with_new_head, self).__init__()
+        self.model_wav = model.wav2vec2
+        self.model_proj = model.projector
+        self.model_class = model.classifier
+        self.new_head = new_head
+
+    def forward(self,x):
+        outputs = self.model_wav(torch.stack( x["input_values"]).permute(1,0).to(device).type(torch.float32))
+        # inter = outputs[0].dot(self.new_head)
+        # #dot prodcut of 3d tensor with 2d tensor
+        inter = torch.einsum('ijk,kl->ijl', outputs[0], self.new_head)
+        
+        inter = self.model_proj(inter)
+        loss = None
+        logits = self.model_class(inter.mean(dim=1))
+        return logits
+org_model = best_model
+best_model = Model_with_new_head(best_model,torch.nn.Parameter(torch.from_numpy(P).float()))
 model_name_extension = "".join(configs)
 model_name = model_checkpoint.split("/")[-1]+model_name_extension+dataset_name
 args = TrainingArguments(
@@ -117,20 +143,36 @@ dataset_validation_combined= concatenate_datasets(
         [encoded_dataset_en,encoded_dataset_cn,encoded_dataset_fr]
     )
 encoded_dataset_validation = dataset_validation_combined.map(preprocess_function,batched=True)
+
+#create dataloader from encoded_dataset_validation
+dataloader = DataLoader(encoded_dataset_validation, batch_size=batch_size, shuffle=False)
+#calculate accuracy and f1 score on validation set using dataloader
 trainer = Trainer(
-    best_model,
+    org_model,
     args,
     eval_dataset=encoded_dataset_validation,
     tokenizer=feature_extractor,
     compute_metrics=compute_metrics
 )
 trainer_f1 = Trainer(
-    best_model,
+    org_model,
     args,
     tokenizer=feature_extractor,
     eval_dataset=encoded_dataset_validation,
     compute_metrics=compute_metrics_f1
 )
+def calculate_accuracy_f1(model,dataloader):
+    model = model.to(device)
+    model.eval()
+    predictions = []
+    labels = []
+    for batch in dataloader:
+        with torch.no_grad():
+            logits = model(batch)
+            predictions.extend(torch.argmax(logits, axis=1).tolist())
+            labels.extend(batch["labels"].tolist())
+    return metric.compute(predictions=predictions, references=labels),metric_f1.compute(predictions=predictions, references=labels,average="weighted")
+print(calculate_accuracy_f1(best_model,dataloader))
 print(trainer.evaluate())
 print("F1")
 print(trainer_f1.evaluate())
